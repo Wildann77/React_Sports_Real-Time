0ee95c158ccddbb646882f68848fcf8e702ea86d
Wildann77 <boyblanco77@gmail.com>
2026-06-05 00:32:19 +0700

    components: export all shared layout components

---
+++ src/shared/components/index.ts
@@ -0,0 +1,17 @@
+export { AdminShell } from './AdminShell';
+export { AppShell } from './AppShell';
+export { EmptyState } from './EmptyState';
+export { ErrorState } from './ErrorState';
+export { GlobalErrorNormalizer } from './GlobalErrorNormalizer';
+export { LoadingState } from './LoadingState';
+export { PageHeader } from './PageHeader';
+export { Backdrop } from './Backdrop';
+export { BrandLogo } from './BrandLogo';
+export { SkipToContent } from './SkipToContent';
+export { ConnectionStatus } from './ConnectionStatus';
+export { AdminSidebar } from './AdminSidebar';
+export { Skeleton } from './Skeleton';
+export { StatCard } from './StatCard';
+export { SectionHeader } from './SectionHeader';
+export { TableRowSkeleton } from './TableRowSkeleton';
+
